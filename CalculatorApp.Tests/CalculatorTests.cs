0b988d6b00b61ef3fbf6f9166c2f77663a882bbc
MichailMishenev <mixayt999@yandex.ru>
2025-04-26 14:39:59 +0300

    sadfsafasdfasdfasdf

--- CalculatorApp.Tests/CalculatorTests.cs
+++ CalculatorApp.Tests/CalculatorTests.cs
@@ -8,7 +8,7 @@ namespace CalculatorApp.Tests
         [Fact]
         public void Add_ReturnsCorrectSum()
         {
-            Calculator calc = new Calculator();
+            Calculator calc = new Calculator(); //asdfsadfasdfasdfasdfadsfasdfsafsadfasdf
             int result = calc.Add(2, 3);
             Assert.Equal(5, result);
         }
